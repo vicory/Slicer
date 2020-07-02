556c641d20d7e910bb9e07fb8bfd42a50fa6500f
Sam Horvath <sam.horvath@kitware.com>
2020-07-02 13:18:13 -0400

    [backport] COMP: Update VTK to fix build with Qt 5.15.0
    $ git shortlog ee1da272ad629bfb9cbf86c51336ee361d095022...a81d1af0825b26946f05f0af9451e993489e801b
    Andras Lasso (2):
          [backport] Add "follow camera direction" orientation mode to vtkGlyph3D
          * [backport MR-6846] BUG: Fix crash in vtkImageHistogram if input contains NaN
    
    Andrew J. P. Maclean (1):
          [backport MR-6943] Qt 5.15 needs the include file QPainterPath
    
    David Gobbi (1):
          [backport] Compatibility for Python 3.8
    
    Jean-Christophe Fillion-Robin (2):
          [backport MR-6754] COMP: Rename CheckCXXSourceRuns CMake module bundled in VTK
          [backport MR-6754] COMP: Rename CHECK_CXX_SOURCE_RUNS to avoid conflict with CMake implementation
    
    Kyle Sunderland (1):
          [backport] Fix bugs in vtkDijkstraGraphGeodesicPath

--- SuperBuild/External_VTK.cmake
+++ SuperBuild/External_VTK.cmake
@@ -122,7 +122,7 @@ if((NOT DEFINED VTK_DIR OR NOT DEFINED VTK_SOURCE_DIR) AND NOT Slicer_USE_SYSTEM
 
 set(_git_tag)
 if("${Slicer_VTK_VERSION_MAJOR}" STREQUAL "8")
-  set(_git_tag "ee1da272ad629bfb9cbf86c51336ee361d095022") # slicer-v8.2.0-2018-10-02-74d9488523
+  set(_git_tag "a81d1af0825b26946f05f0af9451e993489e801b") # slicer-v8.2.0-2018-10-02-74d9488523
 else()
   message(FATAL_ERROR "error: Unsupported Slicer_VTK_VERSION_MAJOR: ${Slicer_VTK_VERSION_MAJOR}")
 endif()
